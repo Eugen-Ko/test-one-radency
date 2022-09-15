988019007e42229a8cd397274c36a9c4882aa4e2
Eugen-Ko <86602482+Eugen-Ko@users.noreply.github.com>
2022-09-15 10:50:38 +0000

    Deploying to gh-pages from @ Eugen-Ko/test-one-radency@7ef13c4c0e36d63fcac1c8cd039e4025098aeffd 🚀

--- index.b6067944.js
+++
@@ -1,2 +0,0 @@
-function t({tagName:t,className:e,attributes:a={}}){const s=document.createElement(t);if(e){const t=e.split(" ").filter(Boolean);s.classList.add(...t)}return Object.keys(a).forEach((t=>s.setAttribute(t,a[t]))),s}(()=>{const e=document.getElementById("root");e.appendChild(t({tagName:"div",className:"test"}))})();
-//# sourceMappingURL=index.b6067944.js.map
