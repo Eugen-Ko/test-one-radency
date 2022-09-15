557a362ed18e9386ee20bd2d995cdfb8a15de771
Eugen <kovalchuk.eak@gmail.com>
2022-09-15 21:52:16 +0300

    small fix in elementStat

--- src/js/components/elementStat.js
+++ src/js/components/elementStat.js
@@ -2,11 +2,11 @@ import { createEl } from '../helpers/createEl';
 import { createCategorySVG } from '../helpers/createCategorySVG';
 
 export const elementStat = ({ category, active, archived }) => {
-  console.log(category, active, archived);
 
   const columns = [category, '', active, '', archived, '', '', ''].map((el, index) => {
     const temp = createEl({
-      tagName: 'p', className: `elementText ${index === 0 ? 'elementText__title' : ''}`, attributes: { id: index === 5 ? `${category}` : '' }
+      tagName: 'p', className: `elementText ${index === 0 ? 'elementText__title' : ''} ${archived && index === 4 ? 'activeArch' : ''}`,
+      attributes: { id: index === 4 ? `${category}` : '' }
     });
     temp.insertAdjacentHTML('beforeend', String(el));
     return temp.outerHTML
